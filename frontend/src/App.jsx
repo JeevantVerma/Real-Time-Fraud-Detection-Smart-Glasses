0ccea008d890c8e0c7f5f23282a5ec6803b01130
Jeevant Verma <jverma_be23@thapar.edu>
2026-05-17 23:40:29 +0530

    Intial frontend with tailwind created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black text-white flex items-center justify-center text-4xl">
+      Tailwind Working
+    </div>
+  );
+}
+
+export default App;
